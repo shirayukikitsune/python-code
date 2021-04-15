67f32a417ac574d21cc909e74922a624a8bd4358
shirayukikitsune <shirayukikitsune@gmail.com>
2021-04-15 00:46:15 -0300

    Add solution for problem 1002

---
+++ src/main/lp/ac_zero/circle_area/main.py
@@ -0,0 +1,11 @@
+def run():
+    r = float(input())
+    pi = 3.14159
+
+    a = pi * r * r
+
+    print('A=%.4f' % a)
+
+
+if __name__ == '__main__':
+    run()
---
+++ src/test/lp/24161/1002/main_test.py
@@ -0,0 +1,44 @@
+import sys
+import unittest
+from unittest.mock import patch, call
+from io import StringIO
+from lp.ac_zero.circle_area import main
+
+
+class MyTestCase(unittest.TestCase):
+    @patch('builtins.print')
+    def test_input_one(self, mocked_print):
+        stdin = sys.stdin
+        sys.stdin = StringIO('2.00\n')
+
+        main.run()
+
+        assert mocked_print.mock_calls == [call('A=12.5664')]
+
+        sys.stdin = stdin
+
+    @patch('builtins.print')
+    def test_input_two(self, mocked_print):
+        stdin = sys.stdin
+        sys.stdin = StringIO('100.64\n')
+
+        main.run()
+
+        assert mocked_print.mock_calls == [call('A=31819.3103')]
+
+        sys.stdin = stdin
+
+    @patch('builtins.print')
+    def test_input_three(self, mocked_print):
+        stdin = sys.stdin
+        sys.stdin = StringIO('150.00\n')
+
+        main.run()
+
+        assert mocked_print.mock_calls == [call('A=70685.7750')]
+
+        sys.stdin = stdin
+
+
+if __name__ == '__main__':
+    unittest.main()
