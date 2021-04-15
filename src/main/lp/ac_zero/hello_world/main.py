4102e0232d355f1b5371e6df3a9671e2ff8d5a72
shirayukikitsune <shirayukikitsune@gmail.com>
2021-04-15 00:43:16 +0000

    Create hello_world project

---
+++ src/main/lp/ac_zero/hello_world/main.py
@@ -0,0 +1,6 @@
+def activity():
+    print("Hello World!")
+
+
+if __name__ == '__main__':
+    activity()
---
+++ src/test/lp/24161/1000/main_test.py
@@ -0,0 +1,15 @@
+import unittest
+from unittest.mock import patch, call
+from lp.ac_zero.hello_world import main
+
+
+class MyTestCase(unittest.TestCase):
+    @patch('builtins.print')
+    def test_activity_output(self, mocked_print):
+        main.activity()
+
+        assert mocked_print.mock_calls == [call('Hello World!')]
+
+
+if __name__ == '__main__':
+    unittest.main()
