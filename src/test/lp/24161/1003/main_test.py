19c4da55b1992a3a4005f37cf52127cc4fa55c81
shirayukikitsune <shirayukikitsune@gmail.com>
2021-04-15 01:17:14 -0300

    Add solution for problem 1003

---
+++ src/test/lp/24161/1003/main_test.py
@@ -0,0 +1,44 @@
+import sys
+import unittest
+from unittest.mock import patch, call
+from io import StringIO
+from lp.ac_zero.simple_sum import main
+
+
+class MyTestCase(unittest.TestCase):
+    @patch('builtins.print')
+    def test_input_one(self, mocked_print):
+        stdin = sys.stdin
+        sys.stdin = StringIO('30\n10\n')
+
+        main.run()
+
+        assert mocked_print.mock_calls == [call('SOMA = 40')]
+
+        sys.stdin = stdin
+
+    @patch('builtins.print')
+    def test_input_two(self, mocked_print):
+        stdin = sys.stdin
+        sys.stdin = StringIO('-30\n10\n')
+
+        main.run()
+
+        assert mocked_print.mock_calls == [call('SOMA = -20')]
+
+        sys.stdin = stdin
+
+    @patch('builtins.print')
+    def test_input_three(self, mocked_print):
+        stdin = sys.stdin
+        sys.stdin = StringIO('0\n0\n')
+
+        main.run()
+
+        assert mocked_print.mock_calls == [call('SOMA = 0')]
+
+        sys.stdin = stdin
+
+
+if __name__ == '__main__':
+    unittest.main()
