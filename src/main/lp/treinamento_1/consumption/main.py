f0cf98de9a97f0863f1a16cb0af2a6359327c47d
Bruno Ferreira <shirayukikitsune@gmail.com>
2021-04-18 06:01:01 -0300

    Add solution for problem 1014

---
+++ src/main/lp/treinamento_1/consumption/main.py
@@ -0,0 +1,9 @@
+def run():
+    x = int(input())
+    y = float(input())
+
+    print('%.3f km/l' % (x / y))
+
+
+if __name__ == '__main__':
+    run()
---
+++ src/test/lp/treinamento_1/consumption/main_test.py
@@ -0,0 +1,15 @@
+import unittest
+from os.path import dirname, abspath
+
+from commons.io.utils import run_tests_with_io
+from lp.treinamento_1.consumption import main
+
+
+class DifferenceTestCase(unittest.TestCase):
+    @run_tests_with_io(abspath(dirname(__file__)) + '/cases')
+    def test_inputs(self):
+        main.run()
+
+
+if __name__ == '__main__':
+    unittest.main()
